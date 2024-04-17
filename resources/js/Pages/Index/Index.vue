dcaf523c7bd360712f1a1ff432ae7ee875d02212
Gadisa Ahmed <gadisaorom@gmail.com>
2024-04-17 18:37:09 +0200

    Initial commit: Basic project setup

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Index Page</h1>
+    <Link href=" /hello">Goto Show Page</Link>
+
+    <p>{{ message }}</p>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+
+defineProps({
+    message: String,
+});
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,14 @@
+<template>
+        <h1>Show Page</h1>
+        <Link href="/">Goto Index Page</Link>
+        <p>{{ message }}</p>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+defineProps({
+    message: String
+})
+
+</script>
+
