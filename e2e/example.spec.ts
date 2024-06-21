7f72a60bf6168d882d7fb28f455b81ddb981706d
Gadisa Ahmed <gadisaorom@gmail.com>
2024-06-22 00:29:51 +0200

    Finalize tests for user registration

--- e2e/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://127.0.0.1:8000/login');
-  await page.getByLabel('Email').click();
-  await page.getByRole('link', { name: 'Login' }).click();
-  await page.getByRole('link', { name: 'Login' }).click();
-});
