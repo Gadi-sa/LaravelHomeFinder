394120b04a3ac9617044e1c75c3ca3bbf8435a69
Gadisa Ahmed <gadisaorom@gmail.com>
2024-07-01 23:43:29 +0200

    Add owner-user relationship to Listing and User models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        \App\Models\Listing::factory(20)->create();
+        \App\Models\Listing::factory(20)->create([
+            'user_id' => 1]);
     }
 }
